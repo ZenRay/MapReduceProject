8ecf9c9a623dcb7c4a5910b91c94f2aeaf41bf68
zenray <jake2011ren@gmail.com>
2024-06-05 22:12:54 +0800

    Init Combiner

---
+++ src/main/java/com/example/mapreduce/combiner/WordCountMapper.java
@@ -0,0 +1,32 @@
+package com.example.mapreduce.combiner;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private Text outKey = new Text();
+    private IntWritable outValue = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        // 1. 获取每行的数据
+        String line = value.toString();
+
+        // 2. 分割数据
+        String[] words = line.split(" ");
+
+        // 3. 循环抛出数据
+        for (String word : words) {
+            outKey.set(word);
+
+            context.write(outKey, outValue);
+        }
+
+
+    }
+}
