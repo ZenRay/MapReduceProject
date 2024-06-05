938d12b3dcb72854f332871af877a564339882d3
zenray <jake2011ren@gmail.com>
2024-06-05 21:58:12 +0800

    update partition sort

--- src/main/java/com/example/mapreduce/partitionsort/Partition.java
+++ src/main/java/com/example/mapreduce/partitionsort/Partition.java
@@ -1,4 +1,43 @@
 package com.example.mapreduce.partitionsort;
 
-public class Partition {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class Partition extends Partitioner<ValueBean, Text> {
+
+    @Override
+    public int getPartition(ValueBean valueBean, Text text, int numPartitions) {
+        /**
+         * 进行 Debug 时发现 text 的之中存在 \u0000 的值进行分割的数据
+         */
+        StringBuilder stringBuilder = new StringBuilder();
+        String phone = text.toString();
+        for(int i=0; i < phone.length(); i++){
+            if(phone.charAt(i) != '\u0000'){
+                stringBuilder.append(phone.charAt(i));
+            }
+        }
+
+        phone = stringBuilder.toString();
+
+
+        String prePhone = phone.substring(0, 3);
+
+        int partition;
+
+        if ("135".equals(prePhone)){
+            partition = 0;
+        } else if ("136".equals(prePhone)) {
+            partition = 1;
+        } else if ("137".equals(prePhone)) {
+            partition = 2;
+        } else if ("138".equals(prePhone)) {
+            partition = 3;
+        } else {
+            partition = 4;
+        }
+
+        return partition;
+
+    }
 }
--- src/main/java/com/example/mapreduce/partitionsort/PartitionSortDriver.java
+++ src/main/java/com/example/mapreduce/partitionsort/PartitionSortDriver.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 /**
  * 本次仅进行了排序，因此我们直接调用计算出来的流量结果数据作为输入。计算的输入需要调用流量统计的结果作为输入，需要删除其他非流量结果数据
+ * 因为进行了分区排序，所以需要设置分区类信息
  */
 public class PartitionSortDriver {
 
@@ -23,8 +24,8 @@ public class PartitionSortDriver {
         job.setJarByClass(PartitionSortDriver.class);
 
         // 3. 关联 Mapper 和 Reducer
-        job.setMapperClass(SortMapper.class);
-        job.setReducerClass(SortReducer.class);
+        job.setMapperClass(PartitionSortMapper.class);
+        job.setReducerClass(PartitionSortReducer.class);
 
         // 4. 设置 Mapper 输出的 Key 和 Value
         job.setMapOutputKeyClass(ValueBean.class);
@@ -34,9 +35,12 @@ public class PartitionSortDriver {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(ValueBean.class);
 
+        job.setPartitionerClass(Partition.class);
+        job.setNumReduceTasks(5);
+
         // 6. 设置输入和输出路径
         FileInputFormat.setInputPaths(job, new Path("./data/output/flowstat"));
-        FileOutputFormat.setOutputPath(job, new Path("./data/output/secondsort"));
+        FileOutputFormat.setOutputPath(job, new Path("./data/output/partitionsort"));
 
         // 7. 提交 Job
         boolean result = job.waitForCompletion(true);
