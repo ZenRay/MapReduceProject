72e450e5e9da3e9af9abfa8cd348e427ec6e1e3b
zenray <jake2011ren@gmail.com>
2024-06-06 22:54:02 +0800

    Update Map Join File

--- src/main/java/com/example/mapreduce/mapjoin/JoinDriver.java
+++ src/main/java/com/example/mapreduce/mapjoin/JoinDriver.java
@@ -1,4 +1,56 @@
 package com.example.mapreduce.mapjoin;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * 当过多的合并流程放在 Reduce 端处理时，非常容易产生数据倾斜。如果某些处理可以放在 Map 端来处理的话
+ * 可以降低 Reduce 短短数据压力，减少数据倾斜。例如需要进行合并的一些数据处理，可以将小文件以 cache 的
+ * 方式存储，在 map 端进行合并
+ */
 public class JoinDriver {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException, ClassNotFoundException {
+        // 1.
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        // 2.
+        job.setJarByClass(JoinDriver.class);
+
+        // 3.
+        job.setMapperClass(JoinMapper.class);
+
+        // 4.
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
+
+        // 5.
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        File cacheFile = new File("./data/input/join/product.txt");
+
+        job.addCacheFile(new URI("file://" + cacheFile.getAbsoluteFile()));
+        // Map 端已经完成了 JOIN ，因此不再需要 Reduce 阶段，将 Reduce 的任务数设置为 0
+        job.setNumReduceTasks(0);
+
+        // 6.
+        FileInputFormat.setInputPaths(job, new Path("./data/input/join/orders.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("./data/output/mapjoin "));
+
+        // 7.
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1 );
+
+    }
 }
--- src/main/java/com/example/mapreduce/mapjoin/JoinMapper.java
+++ src/main/java/com/example/mapreduce/mapjoin/JoinMapper.java
@@ -1,6 +1,59 @@
 package com.example.mapreduce.mapjoin;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class JoinMapper extends Mapper {
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+
+public class JoinMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+    private HashMap<String, String> product = new HashMap<>();
+    private Text outputKey = new Text();
+
+    @Override
+    protected void setup(Mapper<LongWritable, Text, Text, NullWritable>.Context context) throws IOException, InterruptedException {
+        // 获取缓存文件，将文件缓存到集合中
+        URI[] cacheFiles = context.getCacheFiles();
+
+        // 需要将缓存的文件数据，读取出来
+        FileSystem fileSystem = FileSystem.get(context.getConfiguration());
+        BufferedReader reader = null;
+        try{
+            FSDataInputStream inputStream = fileSystem.open(new Path(cacheFiles[0]));
+            reader = new BufferedReader(new InputStreamReader(inputStream, "utf8"));
+
+            String line;
+            while (StringUtils.isNotEmpty(line = reader.readLine())){
+                String[] words = line.split("\t");
+                product.put(words[0], words[1]);
+
+            }
+
+        }finally {
+            // 关闭数据流
+            IOUtils.closeStream(reader);
+        }
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, NullWritable>.Context context) throws IOException, InterruptedException {
+        String string = value.toString();
+
+        String[] words = string.split("\t");
+
+        // 封装数据
+        outputKey.set(words[0] + "\t" + product.get(words[1]) + "\t" + words[2]);
+        context.write(outputKey, NullWritable.get());
+    }
 }
